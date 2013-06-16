7bc0b84df6ef5178ef1ae1cf755e7421e7e3a3c4
John Miner <johnminer@localhost>
2013-06-16 18:24:30 +0000

    Finished animation class
    Animation: Finished class. Not tested (or used yet).
    
    
    svn path=/trunk/; revision=99

--- Header/Animation.hpp
+++ Header/Animation.hpp
@@ -9,11 +9,18 @@
 #define ANIMATION_HPP_
 
 #include <vector>
+#include <string>
 #include <SDL/SDL.h>
+#include "boost/shared_ptr.hpp"
+#include "boost/enable_shared_from_this.hpp"
 #include "RendererElement.hpp"
 #include "Renderer.hpp"
 #include "MasterClockPublisher.hpp"
 #include "MasterClockSubscriber.hpp"
+#include "Dimension.hpp"
+#include "Point.hpp"
+#include "Layer.hpp"
+#include "Graphic.hpp"
 
 class Renderer;
 
@@ -21,17 +28,28 @@ class Animation : public MasterClockSubscriber, public Graphic,
     public boost::enable_shared_from_this<Animation>
 {
 public:
-    Animation();
+    Animation(const Point &newPosition, const Dimension &newSize,
+        const Layer &newLayer);
     Animation(const Animation &rhs);
-    Animation &operation=(const Animation &rhs);
-    void addFrame(std::string &imagePath, Uint32 duration);
+    Animation &operator=(const Animation &rhs);
+    void addFrame(const std::pair<std::string, Uint32> &frame);
     void clockTick(Uint32 elapsedTime);
     void loadImage(Renderer &renderer);
     void draw(boost::shared_ptr<Layout> &layout, Renderer &renderer);
+    void updateFrameIndex();
+    void reposition(const Point &newPosition);
+    void pause(bool shouldPause);
 private:
+    Animation();
     //Each frame of the animation has a path (image name) and a timing
-    std::vector<std::pair<std::string, Uint32 duration> > frames;
+    std::vector<std::pair<std::string, Uint32> > frames;
+    Point position;
+    Dimension size;
+    Layer layer;
     Uint32 currentTicks;
+    Uint32 totalTicks;
+    int frameIndex;
+    bool paused;
 };
 
 #endif
--- Source/Animation.cpp
+++ Source/Animation.cpp
@@ -7,48 +7,94 @@
 
 #include "../Header/Animation.hpp"
 #include "../Header/Renderer.hpp"
+#include "../Header/ImageRendererElement.hpp"
 
-Animation::Animation() : currentTicks(0)
+Animation::Animation(const Point &newPosition, const Dimension &newSize,
+    const Layer &newLayer) : position(newPosition), size(newSize),
+    layer(newLayer), currentTicks(0), totalTicks(0), frameIndex(0),
+    paused(false)
 {
 }
 
-Animation::Animation(const Animation &rhs) : frames(rhs.frames), currentTicks(
-    rhs.currentTicks)
+Animation::Animation(const Animation &rhs) : frames(rhs.frames), 
+    position(rhs.position), size(rhs.size), layer(rhs.layer), currentTicks(
+    rhs.currentTicks), totalTicks(rhs.totalTicks), frameIndex(rhs.frameIndex),
+    paused(rhs.paused)
 {
 }
 
-Animation &Animation::operation=(const Animation &rhs)
+Animation &Animation::operator=(const Animation &rhs)
 {
     if( &rhs == this )
         return *this;
 
     frames = rhs.frames;
+    position = rhs.position;
+    size = rhs.size;
+    layer = rhs.layer;
     currentTicks = rhs.currentTicks;
+    totalTicks = rhs.totalTicks;
+    frameIndex = rhs.frameIndex;
+    paused = rhs.paused;
 
     return * this;
 }
 
+//Animation is not responsible for loading images. This method does nothing
 void Animation::loadImage(Renderer &renderer)
 {
+    //Throw exception?
 }
 
 void Animation::draw(boost::shared_ptr<Layout> &layout, Renderer &renderer)
 {
+    updateFrameIndex();
+    ImageRendererElement re(frames[frameIndex].first,
+        layer.integer(), position, size);
+    layout->drawWhenReady(re);
+}
+
+void Animation::updateFrameIndex()
+{
+    Uint32 currentTotal = 0;
+    frameIndex = 0;
+    int i = 0;
+
+    for( std::vector<std::pair<std::string, Uint32> >::iterator it = 
+        frames.begin(); it != frames.end(); ++it, ++i)
+    {
+        currentTotal += it->second;
+
+        if( currentTicks <= currentTotal )
+            frameIndex = i;
+    }
+}
+
+void Animation::reposition(const Point &newPosition)
+{
+    position.x = newPosition.x;
+    position.y = newPosition.y;
 }
 
 //Assumes image has been loaded
 void Animation::addFrame(const std::pair<std::string, Uint32> &frame)
 {
     frames.push_back(frame);
-    
+    totalTicks += frame.second;
 }
 
 
 void Animation::clockTick(Uint32 elapsedTime)
 {
+    if( !paused )
+    {
+        currentTicks += elapsedTime;
+        currentTicks = Math::cycle(currentTicks, 0, totalTicks);
+    }
 }
 
 void Animation::pause(bool shouldPause)
 {
+    paused = shouldPause;
 }
 
